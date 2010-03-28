903f6b4d2e0ff7a4ce031cccb7f5671d7be0f22e
dr0id <dr0id@bluewin.ch>
2010-03-28 11:21:47 +0200

    added collisiondispatch.py

---
+++ src/player.py
@@ -0,0 +1,12 @@
+import pyglet
+
+
+
+#class Player(pyglet.sprite):
+class Player(object):
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.radius = 10
+
--- src/scenes/game.py
+++ src/scenes/game.py
@@ -1,18 +1,51 @@
 import pyglet
 
 from src.util import img
+from src.player import Player
+from src.collisiondispatch import CollisionDispatcher
+from src.line import Line
 
 # This scene class is the object that the application class maintains
 class GameScene(object):
     def __init__(self, window):
         # Store a reference to the application window
         self.window = window
+        self.coll_funcs = CollisionDispatcher()
+        # add collision functions
+        self.coll_funcs.add(Line, Player, coll_line_player)
+        
+        # entities
+        self.player = Player()
+        self.lines = []
+        
+        # TODO: remove, just for testing
+        self.lines.append(Line(0, 0, 100, 100))
+        
 
     def on_key_press(self, symbol, modifiers):
         pass
+        
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.player.x = x
+        self.player.y = y
 
     def update(self, dt):
-        pass
+        for line in self.lines:
+            if self.coll_funcs.collide(line, self.player):
+                print "colliding", self.player.x, self.player.y
 
     def draw(self):
         pass
+        
+        
+def coll_line_player(line, player):
+    w1 = player.x - line.x1 # vector from p1 to the player
+    w2 = player.y - line.y1
+    # print "w", w1, w2, line.v1, line.v2
+    az = line.v1 * w2 - line.v2 * w1 # cross product
+    a_squared = az * az
+    dist_squared = a_squared / line.length_sq
+    if player.radius * player.radius > dist_squared:
+        return True
+    return False
+
