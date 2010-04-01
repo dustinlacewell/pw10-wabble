23576004a373ff73382da9e87cf49f8ff2ef5c49
dr0id <dr0id@bluewin.ch>
2010-04-02 00:49:39 +0200

    using pyknics skellington

---
+++ run_game_debug.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+TODO: docstring
+"""
+
+__version__ = '$Id: run_game_debug.py 218 2009-07-18 20:44:59Z dr0iddr0id $'
+
+import sys
+import os
+import subprocess
+
+
+# run in right directory
+if not sys.argv[0]:
+    appdir = os.path.abspath(os.path.dirname(__file__))
+else:
+    appdir = os.path.abspath(os.path.dirname(sys.argv[0]))
+os.chdir(appdir)
+if not appdir in sys.path:
+    sys.path.insert(0,appdir)
+
+from src.main import run
+
+def run_debug():
+    # running in debug mode
+    if u"-profile" in sys.argv:
+        import cProfile
+        import tempfile
+        import os
+ 
+        try:
+            cProfile.run('run()', 'profile.txt')
+        finally:
+            pass
+    else:
+        run()
+
+
+if __name__ == '__main__':
+    run_debug()
