6e1e296764236a23513530b8e4c6cf5b57178728
Dustin Lacewell <dlacewell@gmail.com>
2020-06-10 18:45:36 -0500

    Switch pygame to pyglet

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  pyPkgs = python-packages: with python-packages; [ pygame ];
+  pyPkgs = python-packages: with python-packages; [ pyglet ];
   python = pkgs.python2.withPackages pyPkgs;
 in pkgs.writeScriptBin "wabble" ''
    ${python}/bin/python ${./.}/run_game_no_audio.py
